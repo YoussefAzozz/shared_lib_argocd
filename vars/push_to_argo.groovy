f57f793bb88b000b828d6493393dddd5973a4b86
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-19 21:57:00 +0300

    Update push_to_argo.groovy

--- vars/push_to_argo.groovy
+++ vars/push_to_argo.groovy
@@ -1,5 +1,5 @@
 def call(String image_environment) {
-  withCredentials([usernamePassword(credentialsId: '743c416b-fd33-4994-be23-6c1599d6e213', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
+  sshagent(['jenkins-key-git']) {
     sh '''#!/bin/bash
       set -e
       cd argocd-lab
@@ -8,8 +8,7 @@ def call(String image_environment) {
       git config user.name "jenkins-bot"
       git add deployment.yml
       git commit -m "Jenkins update for ${image_environment}:${BUILD_NUMBER}"
-      git config credential.helper '!f() { echo "username=$GIT_USER"; echo "password=$GIT_PASS"; }; f'
-      git push ...
+      git push git@github.com:YoussefAzozz/argocd-lab.git HEAD:master
     '''
   }
 }
