b2f99635bfdc108e4aae5b58d4677fe94657ffda
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-19 22:46:51 +0300

    Update edit_deployment_argocd.groovy

--- vars/edit_deployment_argocd.groovy
+++ vars/edit_deployment_argocd.groovy
@@ -1,9 +1,5 @@
 def call(String image_name) {
-  withCredentials([usernamePassword(
-    credentialsId: '743c416b-fd33-4994-be23-6c1599d6e213', // Update to your actual Jenkins credential ID
-    usernameVariable: 'GIT_USER',
-    passwordVariable: 'GIT_TOKEN'
-  )]) {
+  sshagent(['jenkins-key-git']) {
     sh '''
       git clone https://github.com/YoussefAzozz/argocd-lab.git
     '''
