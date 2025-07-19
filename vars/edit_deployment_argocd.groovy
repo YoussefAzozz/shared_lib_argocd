65894728c19c7fac60c17c3dccfad177f77d23a2
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-20 01:23:03 +0300

    Update edit_deployment_argocd.groovy

--- vars/edit_deployment_argocd.groovy
+++ vars/edit_deployment_argocd.groovy
@@ -5,7 +5,9 @@ def call(String image_name) {
     '''
     dir('argocd-lab') {
     sh """
+    mv argocd-lab argocd-lab-${BUILD_NUMBER}
     # Modify the image line in deployment.yml
+    sed -i 's|replicas:.*|replicas: 3|g' deployment.yml
     sed -i 's|image:.*|image: "${image_name}:${BUILD_NUMBER}"|g' deployment.yml
     cat deployment.yml
     """
