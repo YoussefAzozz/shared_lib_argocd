537b06c632fcdb310a05a928dd0b5c913d2d54e9
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-19 22:03:29 +0300

    Update push_to_argo.groovy

--- vars/push_to_argo.groovy
+++ vars/push_to_argo.groovy
@@ -1,12 +1,20 @@
 def call(String image_environment) {
   sshagent(['jenkins-key-git']) {
-    sh '''
+    sh '''#!/bin/bash
+      set -e
       cd argocd-lab
+
       git config user.email "jenkins@example.com"
       git config user.name "jenkins-bot"
+      
       git add deployment.yml
-      git commit -m "Jenkins update for ${image_environment}:${BUILD_NUMBER}"
-      git push git@github.com:YoussefAzozz/argocd-lab.git HEAD:master
+      
+      if ! git diff --cached --quiet; then
+        git commit -m "Jenkins update for ${image_environment}:${BUILD_NUMBER}"
+        git push git@github.com:YoussefAzozz/argocd-lab.git HEAD:master
+      else
+        echo "No changes to commit"
+      fi
     '''
   }
 }
