d53605ad201b415523909de298a85712916e2660
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-19 23:07:05 +0300

    Update edit_deployment_argocd.groovy

--- vars/edit_deployment_argocd.groovy
+++ vars/edit_deployment_argocd.groovy
@@ -4,11 +4,11 @@ def call(String image_name) {
       git clone https://github.com/YoussefAzozz/argocd-lab.git
     '''
     dir('argocd-lab') {
-    sh '''
+    sh """
     # Modify the image line in deployment.yml
-    sed -i 's|image:.*|image: "$image_name:${BUILD_NUMBER}"|g' deployment.yml
+    sed -i 's|image:.*|image: "${image_name:}${BUILD_NUMBER}"|g' deployment.yml
     cat deployment.yml
-    '''
+    """
   }
   }
 }
