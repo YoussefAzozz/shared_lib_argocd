85072754e0b8a6b949bfe540bc8b31846591d551
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-20 01:24:21 +0300

    Update edit_deployment_argocd.groovy

--- vars/edit_deployment_argocd.groovy
+++ vars/edit_deployment_argocd.groovy
@@ -1,11 +1,10 @@
 def call(String image_name) {
   sshagent(['jenkins-key-git']) {
     sh '''
-      git clone https://github.com/YoussefAzozz/argocd-lab.git
+      git clone https://github.com/YoussefAzozz/argocd-lab.git && mv argocd-lab argocd-lab-${BUILD_NUMBER}
     '''
     dir('argocd-lab') {
     sh """
-    mv argocd-lab argocd-lab-${BUILD_NUMBER}
     # Modify the image line in deployment.yml
     sed -i 's|replicas:.*|replicas: 3|g' deployment.yml
     sed -i 's|image:.*|image: "${image_name}:${BUILD_NUMBER}"|g' deployment.yml
