434544b8b59f5ba8bd3aa2b267808e1cc6c84027
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-19 21:21:20 +0300

    Update push_to_argo.groovy

--- vars/push_to_argo.groovy
+++ vars/push_to_argo.groovy
@@ -1,6 +1,6 @@
 def call(String image_environment) {
   withCredentials([usernamePassword(credentialsId: '743c416b-fd33-4994-be23-6c1599d6e213', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
-    sh '''
+    sh '''#!/bin/bash
       cd argocd-lab
       git config user.email "jenkins@example.com"
       git config user.name "jenkins-bot"
