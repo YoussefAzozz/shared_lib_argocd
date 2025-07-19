92c3c4851d426a46c42645ddfbb65b3b6e5ff8db
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-19 21:01:42 +0300

    Update edit_deployment_argocd.groovy

--- vars/edit_deployment_argocd.groovy
+++ vars/edit_deployment_argocd.groovy
@@ -1,10 +1,27 @@
-def call(String image_environment){
-  withCredentials([usernamePassword(credentialsId: '743c416b-fd33-4994-be23-6c1599d6e213', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
-    sh '''
-    git clone https://${GIT_USER}:${GIT_PASS}@github.com/YoussefAzozz/argocd-lab.git
+def call(String image_name) {
+  withCredentials([usernamePassword(
+    credentialsId: '743c416b-fd33-4994-be23-6c1599d6e213', // Update to your actual Jenkins credential ID
+    usernameVariable: 'GIT_USER',
+    passwordVariable: 'GIT_TOKEN'
+  )]) {
+    sh '''#!/bin/bash
+    set -e
+
+    # Clone the repo
+    git clone https://${GIT_USER}:${GIT_TOKEN}@github.com/YoussefAzozz/argocd-lab.git
     cd argocd-lab
-    sed -i "s|image:.*|image: '$DOCKER_IMAGE:${image_environment}${BUILD_NUMBER}'|g" deployment.yml
+
+    # Modify the image line in deployment.yml
+    sed -i 's|image:.*|image: "${image_tag}"|g' deployment.yml
     cat deployment.yml
+
+    # Commit and push
+    git config user.email "jenkins@example.com"
+    git config user.name "jenkins-bot"
+    git init
+    git add deployment.yml
+    git commit -m "Update image tag to '$image_name':${BUILD_NUMBER}"
+    git push https://${GIT_USER}:${GIT_TOKEN}@github.com/YoussefAzozz/argocd-lab.git HEAD:master
     '''
-}
+  }
 }
