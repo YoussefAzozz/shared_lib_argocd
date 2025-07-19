191dc634020159eff69a120c1ba3879d8152beb8
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-19 20:28:26 +0300

    Update buildAndPushImage.groovy

--- vars/buildAndPushImage.groovy
+++ vars/buildAndPushImage.groovy
@@ -1,5 +1,5 @@
 def call(String imageName, String credentialsId) {
-    withCredentials([usernamePassword(credentialsId: '39efdf06-f686-4219-8117-a9efd32f0c16', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+    withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
         sh """
             docker build -t ${imageName}:${BUILD_NUMBER} .
             echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
