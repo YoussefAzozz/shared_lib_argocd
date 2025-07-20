b8be519a5fdf8cd9e7d9045fd63ceb8f1100d3e7
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-20 18:19:45 +0300

    Update push_to_argo.groovy

--- vars/push_to_argo.groovy
+++ vars/push_to_argo.groovy
@@ -6,7 +6,7 @@ def call(String image_environment) {
         ssh-keyscan github.com >> ~/.ssh/known_hosts
         '''
       
-        dir('argocd-lab') {
+        dir('argocd-lab/overlays/prod') {
           sh '''
             git config user.email "jenkins@example.com"
             git config user.name "jenkins-bot"
