ac8c6fa2bca9d816b6d9b1e6a9dc6cb05b7e74e5
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-20 20:26:46 +0300

    Update edit_deployment_argocd.groovy

--- vars/edit_deployment_argocd.groovy
+++ vars/edit_deployment_argocd.groovy
@@ -7,7 +7,7 @@ def call(String image_name) {
     sh """
     # Modify the image line in deployment.yml
     cd overlays/prod
-    sed -i 's|name: nginx|name: ${image_name}|' kustomization.yml
+    sed -i 's|name: yossefazozz/argocd_lab|name: ${image_name}|' kustomization.yml
     sed -i 's|newTag: "1.25"|newTag: "${BUILD_NUMBER}"|' kustomization.yml
     sed -i 's|replicas: 2|replicas: 4|' patch.yml
     cat patch.yml
