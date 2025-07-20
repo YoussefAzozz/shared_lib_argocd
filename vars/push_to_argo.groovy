1df620a2223a207241565d5e5f12d77f70facd09
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-20 18:16:27 +0300

    Update push_to_argo.groovy

--- vars/push_to_argo.groovy
+++ vars/push_to_argo.groovy
@@ -11,7 +11,7 @@ def call(String image_environment) {
             git config user.email "jenkins@example.com"
             git config user.name "jenkins-bot"
     
-            git add deployment.yml
+            git add kustomization.yml
     
             if ! git diff --cached --quiet; then
               git commit -m "Jenkins update for ${image_environment}:${BUILD_NUMBER}"
